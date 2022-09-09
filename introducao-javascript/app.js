d4f72c7205412c8c2a80961b90a71262a7d865a7
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-09 16:07:28 -0300

    manipulando DOM

--- introducao-javascript/app.js
+++ introducao-javascript/app.js
@@ -1,3 +1,22 @@
 let salvarH1 = document.querySelector(".titulo");
-salvarH1.textContent = "Outro Resultado"
+salvarH1.textContent = "Pacientes Alterados"
 
+//manipulando DOM
+
+let paciente = document.querySelector("#primeiro-paciente")
+let tdpeso = paciente.querySelector(".info-peso");
+let peso = tdpeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura")
+let altura = tdAltura.textContent;
+
+
+let tdImc = paciente.querySelector(".info-imc");
+
+let imcCalc = peso/(altura*altura);
+
+tdImc.textContent = imcCalc;
+
+
+
+console.log(imcCalc);
